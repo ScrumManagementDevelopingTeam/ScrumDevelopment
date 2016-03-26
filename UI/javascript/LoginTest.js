be208ac533ec7eafc751fbe3af4afc616cbe4acf
天运 陈 <RaynorChan@tianyuns-MacBook-Pro.local>
2016-03-26 19:37:51 +0800

    新增 ds store gitignore
    修改功能、接口设计：预留HTTP头 登录令牌的位置

---
+++ UI/javascript/LoginTest.js
@@ -0,0 +1,28 @@
+angular
+    .module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
+    .controller('LoginCtrl', function($scope) {
+        $scope.user = {
+            title: '',
+            email: '',
+            firstName: '',
+            lastName: '',
+            company: 'Google',
+            address: '1600 Amphitheatre Pkwy',
+            city: 'Mountain View',
+            state: 'CA',
+            biography: 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
+            postalCode: '94043'
+        };
+
+        $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
+        'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
+        'WY').split(' ').map(function(state) {
+            return {abbrev: state};
+        })
+    });
+
+
+/**
+ Copyright 2016 Google Inc. All Rights Reserved.
+ Use of this source code is governed by an MIT-style license that can be in foundin the LICENSE file at http://material.angularjs.org/license.
+ **/
